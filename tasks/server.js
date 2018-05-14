4dc1776045cd288097dff4f2f6810b0d5fa4ec74
luodaise <luodaise@icloud.com>
2018-05-14 14:01:52 +0800

    项目构建2

--- tasks/server.js
+++ tasks/server.js
@@ -3,7 +3,7 @@ import gulpif from 'gulp-if';
 import liveserver from 'gulp-live-server';
 import args from './util/args';
 
-gulp.task('serve',(cb)=>{
+gulp.task('server',(cb)=>{
     if(!args.watch) return cb();
 
     var server = liveserver.new(['--harmony','server/bin/www']);
