5569722260c55d4c68693634ab701339bcb5170c
luodaise <luodaise@icloud.com>
2018-05-21 16:12:25 +0800

    剩下 构建

--- app/js/lottery/interface.js
+++ app/js/lottery/interface.js
@@ -1,67 +1,77 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
 class Interface{
     /**
-     * 获取遗漏数据
-     * @param issue {string} {当前期号}
+     * [getOmit 获取遗漏数据]
+     * @param  {string} issue [当前期号]
+     * @return {[type]}       [description]
      */
     getOmit(issue){
-        let self = this
-        return new Promise((resolve, reject) => {
+        let self=this;
+        return new Promise((resolve,reject)=>{
             $.ajax({
                 url:'/get/omit',
                 data:{
                     issue:issue
                 },
-                dataType: 'json',
-                success: function(res){
-                    self.setOmit(res.data)
+                dataType:'json',
+                success:function(res){
+                    self.setOmit(res.data);
                     resolve.call(self,res)
                 },
-                error: function(err){
-                    reject.call(err)
+                error:function(err){
+                    reject.call(err);
                 }
             })
-        })
+        });
     }
-
+    /**
+     * [getOpenCode 获取开奖号码]
+     * @param  {string} issue [期号]
+     * @return {[type]}       [description]
+     */
     getOpenCode(issue){
-        let self = this
-        return new Promise((resolve, reject) => {
+        let self=this;
+        return new Promise((resolve,rejet)=>{
             $.ajax({
-                url: '/get/openCode',
+                url:'/get/opencode',
                 data:{
                     issue:issue
                 },
-                dataType: 'json',
-                success: function(res){
-                    self.setOpenCode(res.data)
-                    resolve.call(self,res)
+                dataType:'json',
+                success:function(res){
+                    self.setOpenCode(res.data);
+                    resolve.call(self,res);
                 },
-                error: function(err){
-                    reject.call(err)
+                error:function(err){
+                    reject.call(err);
                 }
             })
-        })
+        });
     }
 
+    /**
+     * [getState 获取当前状态]
+     * @param  {string} issue [当前期号]
+     * @return {[type]}       [description]
+     */
     getState(issue){
-        let self = this
-        return new Promise((resolve, reject) => {
+        let self=this;
+        return new Promise((resolve,rejet)=>{
             $.ajax({
-                url: '/get/state',
+                url:'/get/state',
                 data:{
                     issue:issue
                 },
-                dataType: 'json',
-                success: function(res){
-                    resolve.call(self,res)
+                dataType:'json',
+                success:function(res){
+                    resolve.call(self,res);
                 },
-                error: function(err){
-                    reject.call(err)
+                error:function(err){
+                    reject.call(err);
                 }
             })
-        })
+        });
     }
 }
 
